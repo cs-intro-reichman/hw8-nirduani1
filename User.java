afe5631e8bf314a0b57455f8959fb8d03467f5be
nirduani1 <nirduani@icloud.com>
2025-01-06 21:13:34 +0200

    test addUser and addFollowee in network class part 8

--- User.java
+++ User.java
@@ -55,6 +55,9 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
+        if (name == null) {
+            return false ;
+        }
         // if this user alredy follows the given name return false
         if (this.follows(name)) {
             return false ;
